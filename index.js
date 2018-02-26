f0e9a2fb4e2cf0486b97b57c676fa7811cacc3c6
shway250 <jackson_duhon@yahoo.com>
2018-02-25 18:43:29 -0800

    giving up on using vanilla API, gonna use the SDK

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()//grab enviroment variables
 var express = require('express');
 var ejsLayouts = require("express-ejs-layouts");
 var bodyParser = require('body-parser');//parse form data into object
+var request = require('request');
+var client = require('smartsheet');
 var app = express();
 
 //configure middleware
@@ -10,11 +12,40 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));//log requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public/'));//configure static assets
+var smartsheet = client.createClient({ accessToken: process.env.ACCESS_TOKEN });
 
 //routes
 app.get('/', function(req, res) {
-  res.render('index', {name: "Sterling Archer"});
-});
+  // var qs = {
+  //   Authorization: 'Bearer '
+  // };
+  // request({
+  //   url:'https://api.smartsheet.com/2.0/sheets',
+  //   qs:qs
+  // }, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     console.log(body) //Show JSON Object.
+  //     res.send(body);
+  //     // res.render('index', {name: ""});
+  //   }else{
+  //     console.log(error);
+  //   }
+  // });
+
+  var options = {
+    id: 3326421302044548 // Id of Sheet
+  };
+
+  // Get sheet
+  smartsheet.sheets.getSheet(options)
+      .then(function(sheetInfo) {
+          console.log(sheetInfo);
+          res.send(sheetInfo)
+      })
+      .catch(function(error) {
+          console.log(error);
+      });
+  });
 
 //server
 app.listen(3000);
